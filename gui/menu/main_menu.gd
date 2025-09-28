39151d646c04994cfbabd9df3b2ff827ae28e9cd
dreemlan <denver@denverjohnston>
2025-09-28 10:54:42 -0700

    Implementing player display names

--- gui/menu/main_menu.gd
+++ gui/menu/main_menu.gd
@@ -1,30 +1,29 @@
 extends CanvasLayer
 
-@onready var button_host: Button = $Control/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ButtonHost
-@onready var button_join: Button = $Control/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ButtonJoin
-@onready var button_settings: Button = $Control/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/ButtonSettings
-@onready var button_quit: Button = $Control/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/ButtonQuit
-
 func _ready() -> void:
-	button_host.pressed.connect(_on_button_host_pressed)
-	button_join.pressed.connect(_on_button_join_pressed)
-	button_settings.pressed.connect(_on_button_settings_pressed)
-	button_quit.pressed.connect(_on_button_quit_pressed)
+	%ButtonHost.pressed.connect(_on_button_host_pressed)
+	%ButtonJoin.pressed.connect(_on_button_join_pressed)
+	%ButtonSettings.pressed.connect(_on_button_settings_pressed)
+	%ButtonQuit.pressed.connect(_on_button_quit_pressed)
 	
 	multiplayer.connected_to_server.connect(_on_connect_to_server)
 
-
-func _on_connect_to_server() -> void:
-	hide()
 
 func _on_button_host_pressed() -> void:
 	if Multiplayer.create_server(): hide()
 
 func _on_button_join_pressed() -> void:
-	Multiplayer.create_client()
+	var client_display_name: String = %LineEditDisplayName.text
+	if client_display_name == "":
+		client_display_name = "Noob"
+	if not Multiplayer.create_client(client_display_name): Helper.log("Failed to create client")
 
 func _on_button_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://gui/menu/settings_menu.tscn")
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_connect_to_server() -> void:
+	hide()
--- multiplayer/multiplayer.gd
+++ multiplayer/multiplayer.gd
@@ -4,35 +4,18 @@ const PORT := 25565
 const IP_ADDRESS := "127.0.0.1"
 const MAX_CLIENTS := 8
 
-var player_list: Dictionary = {}
-
-#
-#func _ready() -> void:
-	#var args = OS.get_cmdline_args()
-	#var is_server = "--server" in args
-	#
-	#if is_server:
-		#var peer = ENetMultiplayerPeer.new()
-		#peer.create_server(PORT, MAX_CLIENTS)
-		#multiplayer.multiplayer_peer = peer
-		#multiplayer.peer_connected.connect(_on_peer_connect)
-		#multiplayer.peer_disconnected.connect(_on_peer_disconnect)
-	#else:
-		#var peer = ENetMultiplayerPeer.new()
-		#peer.create_client(IP_ADDRESS, PORT)
-		#multiplayer.multiplayer_peer = peer
-	#
-	#Helper.log("Multiplayer started")
+var display_name: String = "Noob"
+var peer = ENetMultiplayerPeer.new()
 
 
-func create_client() -> bool:
-	var peer = ENetMultiplayerPeer.new()
+func create_client(client_display_name: String) -> bool:
 	peer.create_client(IP_ADDRESS, PORT)
 	if not peer: return false
 	multiplayer.multiplayer_peer = peer
 	multiplayer.connected_to_server.connect(_on_connect_to_server)
 	multiplayer.connection_failed.connect(_on_failed_to_connect)
 	Helper.log("Attempting to connect to server...")
+	display_name = client_display_name
 	return true
 
 func _on_connect_to_server() -> void:
@@ -43,7 +26,6 @@ func _on_failed_to_connect() -> void:
 
 
 func create_server() -> bool:
-	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT, MAX_CLIENTS)
 	if not peer: return false
 	multiplayer.multiplayer_peer = peer
