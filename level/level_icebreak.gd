ad5f8e13b39bca3bf2bc8124ee8fc527c1548bd6
dreemlan <23227532+Dreemlan@users.noreply.github.com>
2025-10-18 22:38:59 -0700

    Updated icebreak level

--- level/level_icebreak.gd
+++ level/level_icebreak.gd
@@ -21,19 +21,28 @@ func _physics_process(_delta: float) -> void:
 			
 			var last_collision = null
 			if player_can_collide[peer_id] == true:
+				# Set up RNG
+				var rng_num = randi_range(0, 20)
+				# Handle collision
 				var latest_collision = player.get_last_slide_collision()
 				if last_collision != latest_collision:
-					# Get cell coords
-					var cell_coords: Vector2 = tile_map_layer.local_to_map(latest_collision.get_position() - latest_collision.get_normal())
-					# Get tile id using cell coords
-					var tile_id = tile_map_layer.get_cell_source_id(cell_coords)
-					var destroyed_tile_id = 2
-					# Destroy tile
-					if tile_id == 0:
-						rpc("destroy_tile", cell_coords, destroyed_tile_id)
+					var col_rng = randi_range(0, 20)
+					if col_rng == rng_num: 
 					
-					player_can_collide[peer_id] = false
-					last_collision = latest_collision
+						# Get cell coords
+						var cell_coords: Vector2 = tile_map_layer.local_to_map(latest_collision.get_position() - latest_collision.get_normal())
+						
+						# Get tile id using cell coords
+						var tile_id = tile_map_layer.get_cell_source_id(cell_coords)
+						var destroyed_tile_id = 2
+						
+						# Destroy tile
+						if tile_id == 0:
+							rpc("destroy_tile", cell_coords, destroyed_tile_id)
+						
+						# Cleanup
+						player_can_collide[peer_id] = false
+						last_collision = latest_collision
 
 
 @rpc("authority", "call_local", "reliable")
